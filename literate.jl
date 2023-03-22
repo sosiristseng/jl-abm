04d62435d84d538bddc515f17832b6de9b246840
Wen Wei Tseng <sosiristseng@gmail.com>
2023-03-22 21:51:43 +0800

    iterate notebooks

--- literate.jl
+++ literate.jl
@@ -2,3 +2,6 @@ using Literate
 
 Literate.notebook("docs/01-schelling.jl", "docs/")
 Literate.notebook("docs/02-graph-sars2.jl", "docs/")
+Literate.notebook("docs/03-flocking.jl", "docs/")
+Literate.notebook("docs/04-social-distancing.jl", "docs/")
+Literate.notebook("docs/05-zombie.jl", "docs/")
