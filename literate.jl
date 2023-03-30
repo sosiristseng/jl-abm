e776cc066c7e946c39441ed8eea16c66da7927e9
Wen-Wei Tseng <sosiristseng@gmail.com>
2023-03-30 14:33:40 +0800

    CI

--- literate.jl
+++ literate.jl
@@ -1,17 +1,13 @@
 using Distributed
-using Literate
 using PrettyTables
 
-# For all processes
 @everywhere begin
-    ENV["GKSwstype"] = 100
-    import Pkg
-    Pkg.activate(@__DIR__)
+    using Literate
 end
 
-config = Dict("mdstrings" => true)
 folder = joinpath(@__DIR__, "docs")
 nbs = [nb for nb in readdir(folder) if endswith(nb, ".jl")]
+config = Dict("mdstrings" => true, "execute" => true)
 
 ts = pmap(nbs; on_error=ex->NaN) do nb
     @elapsed Literate.notebook(joinpath(folder, nb), folder; config)
@@ -19,4 +15,18 @@ end
 
 pretty_table([nbs ts], header=["Notebook", "Elapsed (s)"])
 
-any(isnan, ts) && error("Error(s) occured!")
+# Retry and locate errors
+for (nb, t) in zip(nbs, ts)
+    if isnan(t)
+        println("Debugging notebook: ", nb)
+        try
+            withenv("JULIA_DEBUG" => "Literate") do
+                Literate.notebook(joinpath(folder, nb), folder; config)
+            end
+        catch e
+            println("Error occured in the cell above")
+        end
+    end
+end
+
+any(isnan, ts) && error("Please checkout errors.")
