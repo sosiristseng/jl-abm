4999867e1afbecde2ecfa022a6667c289cdeed2e
Wen Wei Tseng <sosiristseng@gmail.com>
2024-03-19 08:35:48 +0000

    New CI

--- literate.jl
+++ literate.jl
@@ -1,5 +1,8 @@
+
 using Distributed
 using PrettyTables
+using SHA
+using IJulia
 
 @everywhere begin
     ENV["GKSwstype"] = "100"
@@ -7,34 +10,69 @@ using PrettyTables
     Pkg.activate(Base.current_project())
 end
 
-basedir = "docs"
-config = Dict("mdstrings" => true, "execute" => true)
+outfile = "ipynbs.txt"
+basedir = get(ENV, "DOCDIR", "docs") # Defaults to docs/
+cachedir = get(ENV, "NBCACHE", ".cache") # Defaults to .cache/
+ipynbs = String[]
+litnbs = String[]
 
-nbs = String[]
-
-# Collect the list of Literate notebooks (ends with .jl)
+# Collect the list of notebooks
 for (root, dirs, files) in walkdir(basedir)
     for file in files
-        if (endswith(file, ".jl"))
-            push!(nbs, joinpath(root, file))
+        if endswith(file, ".ipynb") || endswith(file, ".jl")
+            nb = joinpath(root, file)
+            shaval = read(nb, String) |> sha256 |> bytes2hex
+            @info "Notebook $(nb): hash=$(shaval)"
+            shafilename = joinpath(cachedir, root, splitext(file)[1] * ".sha")
+            # Cache hit
+            if isfile(shafilename) && read(shafilename, String) == shaval
+                @info "Notebook $(nb) cache hits and will not be executed."
+            # Cache miss
+            else
+                @info "Notebook $(nb) cache misses. Writing hash to $(shafilename)."
+                mkpath(dirname(shafilename))
+                write(shafilename, shaval)
+                if endswith(file, ".ipynb")
+                    push!(ipynbs, nb)
+                elseif endswith(file, ".jl")
+                    push!(litnbs, nb)
+                end
+            end
         end
     end
 end
 
-# Execute the notebooks in worker process(es)
-ts = pmap(nbs; on_error=ex->NaN) do nb
-    @elapsed Literate.notebook(nb, dirname(nb); config)
+# Remove cached notebook and sha files if there is no respective notebook
+for (root, dirs, files) in walkdir(cachedir)
+    for file in files
+        if endswith(file, ".ipynb") || endswith(file, ".sha")
+            fn = joinpath(joinpath(splitpath(root)[2:end]), splitext(file)[1])
+            nb = fn * ".ipynb"
+            lit = fn * ".jl"
+            if !isfile(nb) && !isfile(lit)
+                fullfn = joinpath(root, file)
+                @info "Notebook $(nb) or $(lit) not found. Removing $(fullfn)."
+                rm(fullfn)
+            end
+        end
+    end
 end
 
-pretty_table([nbs ts], header=["Notebook", "Elapsed (s)"])
+# Execute literate notebooks in worker process(es)
+ts = pmap(litnbs; on_error=ex->NaN) do nb
+    outdir = joinpath(cachedir, dirname(nb))
+    @elapsed Literate.notebook(nb, outdir; mdstrings=true)
+end
+
+pretty_table([litnbs ts], header=["Notebook", "Elapsed (s)"])
 
 # Debug notebooks one by one if there are errors
-for (nb, t) in zip(nbs, ts)
+for (nb, t) in zip(litnbs, ts)
     if isnan(t)
         println("Debugging notebook: ", nb)
         try
             withenv("JULIA_DEBUG" => "Literate") do
-                Literate.notebook(nb, dirname(nb); config)
+                Literate.notebook(nb, dirname(nb); mdstrings=true)
             end
         catch e
             println(e)
@@ -42,4 +80,14 @@ for (nb, t) in zip(nbs, ts)
     end
 end
 
-any(isnan, ts) && error("Please check errors.")
+any(isnan, ts) && error("Please check literate notebook error(s).")
+
+# Install IJulia kernel
+IJulia.installkernel("Julia", "--project=@.", "--heap-size-hint=3G")
+
+# Write the list of jupyter notebooks to be executed
+open(outfile, "w") do f
+    for i in ipynbs
+        println(f, i)
+    end
+end
