adb7a3945a6e400fe1e680e4417e6d3b3f72f4b3
Wen-Wei Tseng <sosiristseng@gmail.com>
2024-04-11 14:42:34 +0800

    Revert "create static folder in cache"
    This reverts commit ad8683526b3dd715ad7dc7cecd1e3d1a54ae1bdb.

--- run.jl
+++ run.jl
@@ -11,9 +11,6 @@ end
 
 basedir = get(ENV, "DOCDIR", "docs") # Defaults to docs/
 cachedir = get(ENV, "NBCACHE", ".cache") # Defaults to .cache/
-mkpath(joinpath(cachedir, basedir, "_static"))
-
-# List of notebooks to be executed
 ipynbs = String[]
 litnbs = String[]
 
